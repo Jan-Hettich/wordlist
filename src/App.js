163fd1e4bfd43abb75c687dc49c4f72ed4df7024
Jan Hettich <janhettich@gmail.com>
2017-05-15 15:07:23 -0700

    added uuid, axios packages. naive implementation working\!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import WordList from './WordLookup';
+import React, {Component} from 'react';
+import WordLookup from './WordLookup';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <WordList />
+        <WordLookup />
       </div>
     );
   }
--- src/WordLookup.js
+++ src/WordLookup.js
@@ -1,17 +1,36 @@
 import React, {Component} from 'react';
+import uuid from 'uuid';
+import axios from 'axios';
 import './WordLookup.css';
 
-class WordList extends Component {
+class WordLookup extends Component {
   constructor(props) {
     super(props);
-    this.state = {firstLetters: ""};
+    this.state = {firstLetters: "", words: []};
   }
 
   textChanged = (event) => {
-    this.setState({firstLetters: event.target.value});
+    const firstLetters = event.target.value;
+    this.setState({firstLetters: firstLetters});
+    this.callAPI(firstLetters);
   }
 
-  render(props) {
+  callAPI(firstLetters) {
+    const url = "https://api.datamuse.com/words?sp=" + firstLetters + "*&max=20";
+    console.log(url);
+    axios.get(url)
+      .then(this.apiCallback)
+      .catch((error) => console.log(error));
+  }
+
+  apiCallback = (api_response) => {
+    //console.log(api_response);
+    const {data} = api_response;
+    const words = data.reduce((acc,{word}) => acc.concat(word), []);
+    this.setState(...this.state, {words})
+  }
+
+  render() {
     return (
       <form className="WordLookup">
         <label>
@@ -22,12 +41,13 @@ class WordList extends Component {
             onChange={this.textChanged}/>
         </label>
         <ul>
-          <li key="1">word1</li>
-          <li key="2">word2</li>
+          {this.state.words.map((word) => (
+            <li key={uuid.v4()}>{word}</li>
+          ))}
         </ul>
       </form>
     );
   }
 }
 
-export default WordList;
+export default WordLookup;
