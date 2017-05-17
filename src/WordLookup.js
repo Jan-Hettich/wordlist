26443d3c7dbadc774fc5e5f9b72a3eff8ee036a2
Jan Hettich <janhettich@gmail.com>
2017-05-17 11:35:12 -0700

    remove delay and sequence number solution to provide a clean starting point

--- src/WordLookup.js
+++ src/WordLookup.js
@@ -7,8 +7,6 @@ class WordLookup extends Component {
   constructor(props) {
     super(props);
     this.state = {firstLetters: "", words: []};
-    this.lastSeqRequest = 0;
-    this.lastSeqResponse = 0;
   }
 
   render() {
@@ -33,10 +31,10 @@ class WordLookup extends Component {
   textChanged = (event) => {
     const firstLetters = event.target.value;
     this.setState({firstLetters: firstLetters});
-    this.callApi(firstLetters);
+    this.updateFromApi(firstLetters);
   }
 
-  callApi(startOfWord) {
+  updateFromApi(startOfWord) {
     axios({
       method: 'get',
       baseURL: 'https://api.datamuse.com',
@@ -44,38 +42,19 @@ class WordLookup extends Component {
       params: {
         sp: startOfWord + '*',
         max: 20,
-        seq: ++this.lastSeqRequest
       },
       responseType: 'json',
     })
-      .then((result1) => this.delay(result1, 1000, () => startOfWord.endsWith('d')))
       .then((result2) => this.apiCallback(result2))
       .catch((error) => console.log(error));
   }
 
   apiCallback = (api_response) => {
     console.log("api_response: ", api_response);
-    this.checkSequenceNumber(api_response);
-    const {data} = api_response;
-    const words = data.reduce((acc, {word}) => acc.concat(word), []);
+    const words = api_response.data.reduce((acc, {word}) => acc.concat(word), []);
     this.setState(...this.state, {words})
   }
 
-  checkSequenceNumber = (api_response) => {
-    const {config: {params: {seq}}} = api_response;
-    console.log(`seq: ${seq}, lastSeqResponse: ${this.lastSeqResponse}`);
-    if (seq <= this.lastSeqResponse) {
-      throw new Error(`discarding response received out of order (${seq})`);
-    }
-    this.lastSeqResponse = seq;
-  }
-
-  delay = (result, ms, condition) => {
-    const delay = condition();
-    console.log(`delay, condition: ${delay}`);
-    ms = delay ? ms : 0;
-    return new Promise((resolve, reject) => setTimeout(() => resolve(result), ms));
-  }
 }
 
 export default WordLookup;
